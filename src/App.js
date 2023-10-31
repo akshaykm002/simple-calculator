34a1db748439b0c6ed4310b942d27d4e6d981c2c
akshaykm002 <akshaykmadhu02@gmail.com>
2023-11-01 00:44:23 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value,setValue]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <div className='backG'>
+  <h1 className='title' >Simple Calculator</h1>
+
+    
+    <div className='calculator'>
+      <form action="">
+        <div>
+          <input type="text" placeholder='0' value={value} />
+
+        </div>
+        <div>
+          <input type="button" value='AC' onClick={()=>setValue('')}/>
+          <input type="button" value='DEL' onClick={()=>setValue(value.slice(0,-1))}/>
+          {/*value.slice(0,-1) is a method used on strings and arrays in JavaScript. It extracts a portion of the string or array and returns it. In this case, it's extracting all elements from the start (0 index) up to, but not including, the last element (-1 index). This effectively removes the last character from value. */}
+          <input type="button" value='.' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='/' onClick={(e)=>setValue(value + e.target.value)}/>
+        </div>
+        <div>
+          <input type="button" value='7' onClick={(e)=>setValue(value + e.target.value)} />
+          <input type="button" value='8' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='9' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='*' onClick={(e)=>setValue(value + e.target.value)}/>
+        </div>
+        <div>
+          <input type="button" value='4' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='5' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='6' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='+'  onClick={(e)=>setValue(value + e.target.value)}/>
+        </div>
+        <div>
+          <input type="button" value='1' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='2' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='3' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='-' onClick={(e)=>setValue(value + e.target.value)}/>
+        </div>
+        <div>
+          <input type="button" value='00' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input type="button" value='0' onClick={(e)=>setValue(value + e.target.value)}/>
+          <input className='equal' type="button" value='=' onClick={()=>setValue(eval(value))}/>
+          {/* here i used eval , a built in function for the calculations */}
+          {/* eval() will evaluate the mathematical expresssion stored in the 'value' */}
+        </div>
+        
+      </form>
     </div>
+  </div>
+  </>
   );
 }
 
