24db08433fd84654510be2892ee6836f79612a13
akshaykm002 <akshaykmadhu02@gmail.com>
2023-11-01 20:35:26 +0530

    calc2

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [value,setValue]=useState('')
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const key = event.key;
+      if ((key >= '0' && key <= '9') || key === '.' || key === '+' || key === '-' || key === '*' || key === '/') {
+        setValue((prevValue) => prevValue + key);
+      } else if (key === 'Enter') {
+        event.preventDefault(); 
+        setValue((prevValue) => eval(prevValue).toString());
+      } else if (key === 'Backspace') {
+        setValue((prevValue) => prevValue.slice(0, -1));
+      } else if (key === 'Escape') {
+        setValue('');
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
-  <>
-  <div className='backG'>
-  <h1 className='title' >Simple Calculator</h1>
+    <>
+      <div className='backG'>
+        <h1 className='title'>Simple Calculator</h1>
 
-    
-    <div className='calculator'>
-      <form action="">
-        <div>
-          <input type="text" placeholder='0' value={value} />
-
+        <div className='calculator'>
+          <form action=''>
+            <div>
+              <input type='text' placeholder='0' value={value} />
+            </div>
+            <div>
+              <input type='button' value='AC' onClick={() => setValue('')} />
+              <input type='button' value='DEL' onClick={() => setValue(value.slice(0, -1))} />
+              <input type='button' value='.' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='/' onClick={(e) => setValue(value + e.target.value)} />
+            </div>
+            <div>
+              <input type='button' value='7' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='8' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='9' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='*' onClick={(e) => setValue(value + e.target.value)} />
+            </div>
+            <div>
+              <input type='button' value='4' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='5' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='6' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='+' onClick={(e) => setValue(value + e.target.value)} />
+            </div>
+            <div>
+              <input type='button' value='1' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='2' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='3' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='-' onClick={(e) => setValue(value + e.target.value)} />
+            </div>
+            <div>
+              <input type='button' value='00' onClick={(e) => setValue(value + e.target.value)} />
+              <input type='button' value='0' onClick={(e) => setValue(value + e.target.value)} />
+              <input className='equal' type='button' value='=' onClick={() => setValue(eval(value).toString())} />
+            </div>
+          </form>
         </div>
-        <div>
-          <input type="button" value='AC' onClick={()=>setValue('')}/>
-          <input type="button" value='DEL' onClick={()=>setValue(value.slice(0,-1))}/>
-          {/*value.slice(0,-1) is a method used on strings and arrays in JavaScript. It extracts a portion of the string or array and returns it. In this case, it's extracting all elements from the start (0 index) up to, but not including, the last element (-1 index). This effectively removes the last character from value. */}
-          <input type="button" value='.' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='/' onClick={(e)=>setValue(value + e.target.value)}/>
-        </div>
-        <div>
-          <input type="button" value='7' onClick={(e)=>setValue(value + e.target.value)} />
-          <input type="button" value='8' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='9' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='*' onClick={(e)=>setValue(value + e.target.value)}/>
-        </div>
-        <div>
-          <input type="button" value='4' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='5' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='6' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='+'  onClick={(e)=>setValue(value + e.target.value)}/>
-        </div>
-        <div>
-          <input type="button" value='1' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='2' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='3' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='-' onClick={(e)=>setValue(value + e.target.value)}/>
-        </div>
-        <div>
-          <input type="button" value='00' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input type="button" value='0' onClick={(e)=>setValue(value + e.target.value)}/>
-          <input className='equal' type="button" value='=' onClick={()=>setValue(eval(value))}/>
-          {/* here i used eval , a built in function for the calculations */}
-          {/* eval() will evaluate the mathematical expresssion stored in the 'value' */}
-        </div>
-        
-      </form>
-    </div>
-  </div>
-  </>
+      </div>
+    </>
   );
 }
 
